6b6f4c1ff946432342aab3b8ae096d6694a30a92
nzyoni <yoni.naim@logz.io>
2018-12-30 20:28:58 +0200

    Notifications level up

--- src/pages/notifications/Notification.jsx
+++ src/pages/notifications/Notification.jsx
@@ -1,25 +1,38 @@
 /*global chrome*/
 import React from 'react';
 import * as moment from "moment";
+import { Grid, Paper}  from "@material-ui/core";
+
+const containerStyle ={
+    marginBottom: '5px',
+};
 
 const titleStyle = {
     fontSize: '15px',
     color: '#0B7CAD',
+    textDecoration: 'none',
 };
 
 const timestampStyle = {
     fontSize: '15px',
     color: '#797D82',
-
+    align: 'right',
 };
 
+const editAlert = (id) => `https://app.logz.io/#/dashboard/alerts/optimizer-wizard/${id}`;
 const toDate = (unixTime) => moment.unix(unixTime).format('HH:mm:ss');
 
 export const Notification = (props) => {
     return (
         <div>
-            <lable style={titleStyle}>{props.title}</lable>
-            <label style={timestampStyle}>{toDate(props.timestamp)}</label>
+            <Grid container spacing={12} style={containerStyle}>
+                <Grid item xs={9} style={titleStyle}>
+                    <a style={titleStyle} target="_blank" href={editAlert(props.id)}>{props.title}</a>
+                </Grid>
+                <Grid item xs={3} style={timestampStyle}>
+                    <label>{toDate(props.timestamp)}</label>
+                </Grid>
+            </Grid>
         </div>
     );
 };
--- src/pages/notifications/NotificationsPage.jsx
+++ src/pages/notifications/NotificationsPage.jsx
@@ -1,6 +1,7 @@
 /*global chrome*/
 import React from "react";
 import {Notification} from "./Notification";
+import Switch from "@material-ui/core/Switch/Switch";
 
 const alertsHeader = {
     fontSize: '14px',
@@ -11,19 +12,41 @@ const alertsBody = {
     margin: '30px',
 };
 
+const alertsFooter = {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+};
+
 export class NotificationsPage extends React.Component {
     state = {
+        notificationsActive: null,
         triggeredAlerts: [],
     };
 
     componentWillMount() {
         chrome.storage.sync.get('triggeredAlerts',
             ({triggeredAlerts}) => this.setState({triggeredAlerts: triggeredAlerts.results}));
+        chrome.storage.sync.get('NotificationsActive',
+            ({ NotificationsActive }) => {
+                console.log('notificationsActive -', NotificationsActive);
+                this.setState({'notificationsActive': (NotificationsActive !== false)});
+            });
     }
 
+    handleChange = name => evt => {
+        chrome.storage.sync.set({ 'NotificationsActive': evt.target.checked}, () => {
+            this.setState({ [name]: evt.target.checked });
+        });
+    };
+    // handleChange = name => event => {
+    //     this.setState({ [name]: event.target.checked });
+    // };
+
     render() {
-        const notifications = this.state.triggeredAlerts.map(
-            (alert) => <Notification title={alert.name} timestamp={alert.eventDate}/>);
+        const notifications = this.state.triggeredAlerts.reverse().map(
+            (alert) => <Notification id={alert.alertId} title={alert.name} timestamp={alert.eventDate}/>);
+
         return (
             <div>
                 <div style={alertsHeader}>
@@ -31,6 +54,21 @@ export class NotificationsPage extends React.Component {
                 </div>
                 <div style={alertsBody}>
                     {notifications}
+                </div>
+                <div>
+                    STATE:
+                    <br/>
+                    {JSON.stringify(this.state)}
+                </div>
+                <div style={alertsFooter}>
+                    <Switch
+                        checked={this.state.notificationsActive}
+                        onChange={this.handleChange('notificationsActive')}
+                        color="primary"
+                        value="notificationsActive"
+                    />
+                    Turn notifications
+                    <label>{this.state.notificationsActive ? ' off' : ' on'}</label>
                 </div>
             </div>
         );
