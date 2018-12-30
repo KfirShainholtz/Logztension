e7b26460e7a3a58e88827ecbdd798f98cfa714be
michael.yakubov.1985@gmail.com <michael.y@logz.io>
2018-12-30 15:09:52 +0200

    Support notifications, export notifications code

--- public/app/background.js
+++ public/app/background.js
@@ -11,15 +11,19 @@ var requestData = JSON.stringify({
     "sortOrder": "ASC"
 });
 
+function updateBadge(triggeredAlertsResponse) {
+    chrome.storage.sync.set({'triggeredAlerts': triggeredAlertsResponse});
+    chrome.browserAction.setBadgeText({"text": String(triggeredAlertsResponse.results.length)});
+    chrome.browserAction.setBadgeBackgroundColor({color: 'red'});
+}
+
 function handleTriggeredAlerts() {
     if (this.readyState === XMLHttpRequest.DONE) {
         if(this.status === 200) {
             var response = JSON.parse(this.response);
-            chrome.storage.sync.set({'triggeredAlerts': response});
-            chrome.browserAction.setBadgeText({"text": String(response.results.length)});
-            chrome.browserAction.setBadgeBackgroundColor({color: 'red'});
+            updateBadge(response);
         }
-        setTimeout(() => getLatestTriggeredAlerts(), 2000);
+        setTimeout(() => getLatestTriggeredAlerts(), 5000);
     }
 }
 
@@ -28,7 +32,7 @@ function getLatestTriggeredAlerts() {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', triggeredAlertsUrl, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('X-API-TOKEN', '8c76f241-84f0-4a82-b552-60a80ebb9a19');
+    xhr.setRequestHeader('X-API-TOKEN', 'b5c906c8-7c72-4364-b5a1-0ed5c74ce185');
     xhr.onreadystatechange = handleTriggeredAlerts;
     xhr.send(requestData);
 }
